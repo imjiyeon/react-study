ff8127d06f2c131a128be1b9bd67f88dc3964cdc
임지연 <yoyt12@naver.com>
2024-10-28 22:22:18 +0900

    ㄴㅇㄴㅇ

--- react-jsx-example/src/App1.js
+++ react-jsx-example/src/App1.js
@@ -1,20 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-// react 패키지에서 React 가져오기
+// react 패키지에서 React 객체 가져오기
 import React from 'react';
 
-// jsx 문법으로 HTML 코드 작성하기
-// 자바스크립트와 HTML 코드가 결합된 것을 JSX문법이라고 함
+// HTML 코드 작성하기
 
 function App() {
 
-  // createElement 함수로 돔 생성
-  // 태그, props, 컨텐츠
+  // 1.자바스크립트의 createElement 함수로 HTML 태그 생성
+  // 인자: 태그종류, props, 컨텐츠(내용)
   let element1 = React.createElement("div", null, "Hello");
-  // jsx 방식으로 돔 생성
-  // jsx 방식을 사용하면 내부적으로는 createElement함수가 호출되어 결과가 같음
+
+  // 2.리액트의 jsx로 HTML 태그 생성
+  // jsx 방식을 사용하면 createElement함수로 변환되어 결과가 같음
   let element2 = <div>Hello</div>
 
+  // JSX 안에서 변수를 사용할 때는 {} 중괄호 문법 사용
+
+  // 필요없는 태그 모두 삭제
   return (
     <div>
       {element1}
