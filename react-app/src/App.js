e071fa8d2f1dca4ab74df50d4ea8ee47c2f2f565
임지연 <yoyt12@naver.com>
2024-10-28 22:05:00 +0900

    sdd

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,25 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
+// 전체 화면을 구성하는 App 함수
 // 입력필드, 버튼 같은 UI를 작성하는 부분
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello React!
     </div>
   );
 }
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// App.js 파일에서 App이라는 컴포넌트 가져오기
+import App from './App'; 
 import reportWebVitals from './reportWebVitals';
 
 // <React.StrictMode>은 코드를 검사해주는 부분으로, 문제가 있으면 표시
 // 문제가 있으면 브라우저의 콘솔창에 표시
 
+// App이라는 컴포넌트를 포함하고 있음
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App /> 
+    <App /> {/* 제거 */}
   </React.StrictMode>
 );
 
