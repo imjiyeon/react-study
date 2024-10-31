425fde71d947fcb08ba4bd2b63081bcbff737267
임지연 <yoyt12@naver.com>
2024-11-01 00:42:01 +0900

    ㅇㅇㅇ

--- react-quiz-movie-app/src/App.js
+++ react-quiz-movie-app/src/App.js
@@ -6,6 +6,7 @@ import Movies from './component/Movies';
 import MovieDetail from './component/MovieDetail';
 
 function App() {
+  // 메뉴는 App에 추가하여 고정시키기
   return (
     <div>
       <ul>
@@ -13,13 +14,14 @@ function App() {
           <NavLink to="/" end>Home</NavLink>
         </li>
         <li>
-          <NavLink to="/movie/list">Movies</NavLink>
+          <NavLink to="/movies">Movies</NavLink>
         </li>
       </ul>
 
+      {/* URL경로에 따라 웰컴, 영화목록, 영화상세 화면 표시 */}
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/movie/list" element={<Movies />} />
+        <Route path="/movies" element={<Movies />} />
         <Route path="/movie/:movieId" element={<MovieDetail />} />
       </Routes>
     </div>
