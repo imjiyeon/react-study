29161b8f949e791d682c1073f689ca68fdff3481
임지연 <yoyt12@naver.com>
2024-11-03 23:59:48 +0900

    ddd

--- react-usereducer-example/src/App.js
+++ react-usereducer-example/src/App.js
@@ -1,57 +1,42 @@
 import './App.css';
-import { useState, useReducer } from 'react';
+// react 패키지에서 useState 함수 가져오기
+import { useState } from 'react';
 
+// useState를 사용하여 카운터 앱 만들기
+// 버튼을 클릭하면 0 부분이 바뀌도록 처리
 
-// 텍스트 상자를 추가하여 사용자가 입력한 숫자만큼 카운터 값을 증가/감소
+// useState란? 상태값을 만드는 함수
+// 값이 변경되면 컴포넌트가 새로 렌더링됨
 
+// 불필요한 코드 제거
 function App() {
 
-  // action.type에 따라 상태를 변경, action.num만큼 증가 또는 감소
-  function countReducer(oldCount, action){
+  // 0을 초기값으로 하는 상태 만들기
+  // 상태값, 상태를 변경하는 함수
+  const [count, setCount] = useState(0);
 
-    if(action.type === 'UP'){
-      return oldCount + action.num;    
-    } else if(action.type === 'DOWN'){
-      return oldCount - action.num;  
-    } else if(action.type === 'RESET'){
-      return 0;  
-    }
-
+  function down() {
+    setCount(count - 1);
   }
 
-  const [count, countDispatch] = useReducer(countReducer, 0);
-
-  // 텍스트 상자를 통해 입력받은 값을 state로 관리
-  const [num, setNum] = useState(0);
-
-  // 사용자가 텍스트 상자에 값을 입력할 때마다 호출되는 함수
-  // 입력된 값을 숫자로 변환하여 num 상태를 업데이트
-  function changeNum(event){
-    setNum(Number(event.target.value));
+  function reset() {
+    setCount(0);
   }
 
-  // countDispatch를 호출하여 액션과 증가해야할 값 전달
-  function down(){
-    countDispatch({ type:'DOWN', num: num});
+  function up() {
+    setCount(count + 1);
   }
 
-  function reset(){
-    countDispatch({ type:'RESET', num: num});
-  }
-
-  function up(){
-    countDispatch({ type:'UP', num: num});
-  }
-
+  // + 버튼을 누르면 카운터를 1 증가시키고 - 버튼을 누르면 1 감소시키기
   return (
-    <div>
+    <div class='App' >
       <input type='button' value="-" onClick={down}></input>
       <input type='button' value="0" onClick={reset}></input>
       <input type='button' value="+" onClick={up}></input>
-      <input type='text' value={num} onChange={changeNum} ></input>
       <span>{count}</span>
     </div>
   );
 }
+
 
 export default App;
