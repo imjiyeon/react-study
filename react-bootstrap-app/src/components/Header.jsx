9bdbc40eb43e7bbbb5485337ebe1f6e174d4fcb7
임지연 <yoyt12@naver.com>
2024-11-03 14:40:14 +0900

    ㅇㅇ

--- react-bootstrap-app/src/components/Header.jsx
+++ react-bootstrap-app/src/components/Header.jsx
@@ -17,17 +17,18 @@ const HeaderContainer = styled.div`
 const Header = () => {
   return (
     <HeaderContainer>
-      <Navbar collapseOnSelect expand="lg">
+      {/* Navbar 클래스 삭제 */}
+      <Navbar expand="lg">
         <Container>
           <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="/login">login</Nav.Link>
-              <Nav.Link href="/logout">logout</Nav.Link>
-              <Nav.Link href="/register">register</Nav.Link>
-              <Nav.Link href="/">home</Nav.Link>
-              <Nav.Link href="/board/list">board</Nav.Link>
+                <Nav.Link href="/login">login</Nav.Link>
+                <Nav.Link href="/logout">logout</Nav.Link>
+                <Nav.Link href="/register">register</Nav.Link>
+                <Nav.Link href="/">home</Nav.Link>
+                <Nav.Link href="/board/list">board</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
